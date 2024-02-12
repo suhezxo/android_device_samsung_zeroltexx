c53340e9e954c2c1b7d4dabbd65de80c031c7723
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2024-02-12 19:07:09 +0000

    zero: Add lifevibes symlink

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -11,8 +11,6 @@ set -e
 VENDOR=samsung
 DEVICE=zerofltexx
 
-export INITIAL_COPYRIGHT_YEAR=2015
-
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
@@ -33,6 +31,29 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 write_headers
 
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+###################################################################################################
+# CUSTOM PART START                                                                               #
+###################################################################################################
+OUTDIR=vendor/$VENDOR/$DEVICE
+(cat << EOF) >> $ANDROID_ROOT/$OUTDIR/Android.mk
+include \$(CLEAR_VARS)
+
+LIFEVIBES_LIBS := libLifevibes_lvverx.so libLifevibes_lvvetx.so
+
+LIFEVIBES_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib/,\$(notdir \$(LIFEVIBES_LIBS)))
+\$(LIFEVIBES_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
+	@echo "LifeVibes lib link: \$@"
+	@mkdir -p \$(dir \$@)
+	@rm -rf \$@
+	\$(hide) ln -sf /vendor/lib/soundfx/\$(notdir \$@) \$@
+
+ALL_DEFAULT_INSTALLED_MODULES += \$(LIFEVIBES_SYMLINKS)
+
+EOF
+###################################################################################################
+# CUSTOM PART END                                                                                 #
+###################################################################################################
 
 # Done
 write_footers
