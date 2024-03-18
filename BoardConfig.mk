cb1dbd9ed1555898092e9804114bc1b419d0592b
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2024-03-18 14:37:54 +0000

    Commonize shims
    all devices that are supported by this tree need this

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,16 +28,6 @@ TARGET_OTA_ASSERT_DEVICE := zeroflte,zerofltexx,zeroflteskt
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
-# Camera-shims
-TARGET_LD_SHIM_LIBS += \
-	/vendor/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
-	/vendor/lib64/libexynoscamera.so|/vendor/lib64/libexynoscamera_shim.so
-
-# Fingerprint-shims
-TARGET_LD_SHIM_LIBS += \
-	/vendor/lib/libbauthserver.so|/vendor/lib/libbauthtzcommon_shim.so \
-	/vendor/lib64/libbauthserver.so|/vendor/lib64/libbauthtzcommon_shim.so
-
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += $(DEVICE_PATH)/include
 
